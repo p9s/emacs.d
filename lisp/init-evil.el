c2c423c6b9dcf348d2f3cabb8672e73fe6abf0dc
mc <mc@appledeMacBook-Pro.local>
2019-08-15 23:38:30 +0800

    bind ctrl + g to alias esc key

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -11,5 +11,18 @@
 ;;              (global-evil-surround-mode 1))
 
 
+(defun evil-keyboard-quit ()
+  "Keyboard quit and force normal state."
+  (interactive)
+  (and evil-mode (evil-force-normal-state))
+  (keyboard-quit))
+
+(define-key evil-normal-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-motion-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-insert-state-map   (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-window-map         (kbd "C-g") #'evil-keyboard-quit)
+(define-key evil-operator-state-map (kbd "C-g") #'evil-keyboard-quit)
+
+
 (provide 'init-evil)
 ;;; init-evil.el ends here
